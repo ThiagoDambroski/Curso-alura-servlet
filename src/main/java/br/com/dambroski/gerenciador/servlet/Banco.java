6d1042df9c4ec10b18eb51ef4a5c085e507d97b3
ThiagoDambroski <dambroskic@hotmail.com>
2022-07-26 15:33:54 -0300

    delete

--- src/main/java/br/com/dambroski/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/dambroski/gerenciador/servlet/Banco.java
@@ -1,26 +1,45 @@
 package br.com.dambroski.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa(null, "Google");
+		empresa.setId(chaveSequencial++);
 		Empresa empresa2 = new Empresa(null,"Aura");
+		empresa2.setId(chaveSequencial++);
 		Banco.lista.add(empresa);
 		Banco.lista.add(empresa2);
 	}
 
 	public void adiciona(Empresa empresa) {
+		empresa.setId(chaveSequencial++);
 		Banco.lista.add(empresa);
 		
 	}
 	
 	public List<Empresa> getEmpresa(){
 		return Banco.lista;
+	}
+
+	public void remove(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+			}
+			
+		}
+		
 	}
 
 }
